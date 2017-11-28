6f84b6f1e5ee4a2cebc4d0e7e48c9d3cd5e0d389
ajaegge <andrea.jaegge@gmail.com>
2017-11-28 12:39:22 -0600

    Subset data

--- FinalProject_script.R
+++ FinalProject_script.R
@@ -1,11 +1,30 @@
 #testing
 
-P <- The_Australian_Phytoplankton_Database_1844_2016_abundance_and_biovolume_1_
+P <- as.data.frame(The_Australian_Phytoplankton_Database_1844_2016_abundance_and_biovolume_1_)
 
-as.numeric(P$BIOVOLUME_UM3_L) 
+P$BIOVOL <- P$BIOVOLUME_UM3_L
+
+#Subset data
+x <- P[complete.cases(P$BIOVOL), ]
+
+#P$BIOVOL
+
+#P[is.na(P$BIOVOL)] <- 0
+
+subset(P, P$BIOVOL!="NA")
+
+P$BIOVOL
+
+as.numeric(P$BIOVOLUME_UM3_L)
+
+finalData <- subset(P,!(is.na(data["BIOVOL"])))
+
+ 
 
 #subset data
-finalData<-subset(P,(is.na(data["P$BIOVOLUME_UM3_L"])))
+PhytoB <-subset(P, )
+
+bar <- subset(foo, location == "there")
 
 
 
