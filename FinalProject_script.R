58ec45717ea2a3ef36f5d42eb427eff0f6fcd243
ajaegge <andrea.jaegge@gmail.com>
2017-11-28 12:10:28 -0600

    trying to subset data

--- FinalProject_script.R
+++ FinalProject_script.R
@@ -1,1 +1,11 @@
 #testing
+
+P <- The_Australian_Phytoplankton_Database_1844_2016_abundance_and_biovolume_1_
+
+as.numeric(P$BIOVOLUME_UM3_L) 
+
+#subset data
+finalData<-subset(P,(is.na(data["P$BIOVOLUME_UM3_L"])))
+
+
+
